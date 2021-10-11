f25d02e1ca2d87bd16e2e7fbb519b074122e819e
Noorku <noorskoulayb@gmail.com>
2021-10-10 22:13:37 -0700

    Delete TodoItem from the data

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -63,7 +63,7 @@
   <!-- End 1st Face -->
   <!-- Start 2nd Face -->
   <div
-    v-for="adding in allTodos"
+    v-for="adding  in allTodos"
     :key="adding.id"
     class="
       main-div
@@ -119,9 +119,10 @@
       <!-- side icons -->
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mx-5"
+        class="h-5 w-5 mx-5 cursor-pointer"
         viewBox="0 0 20 20"
         fill="currentColor"
+      
       >
         <path
           d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -129,7 +130,7 @@
       </svg>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mx-2"
+        class="h-5 w-5 mx-2 cursor-pointer"
         viewBox="0 0 20 20"
         fill="currentColor"
       >
@@ -141,9 +142,11 @@
       </svg>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mx-5"
+        class="h-5 w-5 mx-5 cursor-pointer"
         viewBox="0 0 20 20"
         fill="currentColor"
+        @click="deleteTodo(adding)"
+        
       >
         <path
           fill-rule="evenodd"
@@ -171,7 +174,6 @@
   <div class="add">
     <button @click="addpost">AddNewTodo</button>
   </div>
-
   <!-- <h1>AllTODOS: {{ AllTODOSItems }}</h1> -->
 </template>
 
@@ -291,6 +293,10 @@ export default {
         console.log("checked");
       }
     },
+    deleteTodo(adding) {
+     this.allTodos.splice(this.allTodos.indexOf(adding), 1)
+     console.log('delete')
+     }
   },
   computed: {
     AllTODOSItems() {
@@ -298,6 +304,7 @@ export default {
     },
   },
 };
+
 </script>
 
 <style>
